eac615551abe2950d9bd15ed202d9e40b53f9621
Michael Green <57341710+michaelgreenl@users.noreply.github.com>
2026-01-23 20:40:54 -0500

    config: migrate capacitor config from .ts to .json"

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -4,7 +4,21 @@ module.exports = {
         'type-enum': [
             2,
             'always',
-            ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'wip'],
+            [
+                'build',
+                'chore',
+                'ci',
+                'config',
+                'docs',
+                'feat',
+                'fix',
+                'perf',
+                'refactor',
+                'revert',
+                'style',
+                'test',
+                'wip',
+            ],
         ],
     },
 };
