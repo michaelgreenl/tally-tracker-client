66cd7097f07bdd60ba30cf7227822c656751c0c5
Michael Green <57341710+michaelgreenl@users.noreply.github.com>
2026-01-23 20:46:43 -0500

    ci: configured vite proxy server for native development

--- vite.config.ts
+++ vite.config.ts
@@ -26,4 +26,39 @@ export default defineConfig({
         globals: true,
         environment: 'jsdom',
     },
+    server: {
+        port: 8100,
+        host: true,
+        proxy: {
+            '/users': {
+                target: 'https://tally-tracker-server.onrender.com',
+                changeOrigin: true,
+                secure: true,
+                cookieDomainRewrite: 'localhost',
+                configure: (proxy, _options) => {
+                    proxy.on('error', (err, _req, _res) => {
+                        console.log('proxy error', err);
+                    });
+                    proxy.on('proxyReq', (proxyReq, req, _res) => {
+                        console.log('Sending Request to the Target:', req.method, req.url);
+                    });
+                    proxy.on('proxyRes', (proxyRes, req, _res) => {
+                        console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+                    });
+                },
+            },
+            '/counters': {
+                target: 'https://tally-tracker-server.onrender.com',
+                changeOrigin: true,
+                secure: true,
+                cookieDomainRewrite: 'localhost',
+            },
+            '/health': {
+                target: 'https://tally-tracker-server.onrender.com',
+                changeOrigin: true,
+                secure: true,
+                cookieDomainRewrite: 'localhost',
+            },
+        },
+    },
 });
