5f47b1469689f37266b5967ee0b3dfdb91d80c32
Michael Green <57341710+michaelgreenl@users.noreply.github.com>
2026-02-15 15:26:48 -0500

    test: adding commands and status-codes to e2e support

--- tests/e2e/support/commands.ts
+++ tests/e2e/support/commands.ts
@@ -1,37 +1,38 @@
-/// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+declare global {
+    namespace Cypress {
+        interface Chainable {
+            login(email: string, password: string, rememberMe?: boolean): Chainable<void>;
+            loginAsGuest(): Chainable<void>;
+        }
+    }
+}
+
+Cypress.Commands.add('login', (email: string, password: string, rememberMe = false) => {
+    cy.intercept('POST', '/users/login').as('loginReq');
+
+    cy.visit('/login');
+
+    cy.get('.ion-page:not(.ion-page-hidden)').should('be.visible');
+
+    cy.get('input[type="email"]').type(email);
+    cy.get('input[type="password"]').type(password);
+
+    if (rememberMe) {
+        cy.get('ion-toggle').click();
+    }
+
+    cy.contains('ion-button', 'Login').click();
+    cy.wait('@loginReq');
+    cy.url().should('include', '/home');
+});
+
+Cypress.Commands.add('loginAsGuest', () => {
+    cy.visit('/login');
+
+    cy.get('.ion-page:not(.ion-page-hidden)').should('be.visible');
+
+    cy.get('.ion-page:not(.ion-page-hidden)').contains('Continue as guest').click();
+    cy.url().should('include', '/home');
+});
+
+export {};
