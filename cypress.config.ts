8f99e35a5958ea68d8aa5981ea58a403cb61b148
Michael Green <57341710+michaelgreenl@users.noreply.github.com>
2026-02-15 13:21:24 -0500

    config(cypress): adding video/screenshot folders

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
     e2e: {
+        videosFolder: 'tests/e2e/videos',
+        screenshotsFolder: 'tests/e2e/screenshots',
         supportFile: 'tests/e2e/support/e2e.{js,jsx,ts,tsx}',
         specPattern: 'tests/e2e/specs/**/*.cy.{js,jsx,ts,tsx}',
         baseUrl: 'http://localhost:8100',
